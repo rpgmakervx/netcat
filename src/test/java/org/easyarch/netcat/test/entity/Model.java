030f693ddb58acf28a7dae58b3770c8ce8f4fdcf
rpgmakervx <583110127@qq.com>
2017-03-24 10:58:38 +0800

    form data wrapper

---
+++ src/test/java/org/easyarch/netcat/test/entity/Model.java
@@ -0,0 +1,13 @@
+package org.easyarch.netcat.test.entity;
+
+/**
+ * Created by xingtianyu on 17-3-23
+ * 下午3:55
+ * description:
+ */
+
+public class Model<T> {
+
+    public static void main(String[] args) {
+    }
+}
